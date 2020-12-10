18133a673fc9d2023c6f0d7da2078c016701269b
Yifan Hong <elsk@google.com>
2020-12-10 12:14:58 -0800

    sm8475-common: Enforce generic ramdisk allow list
    This change enforces that only the list of files on the generic
    ramdisk can be installed to the ramdisk of this device.
    
    This is required for all devices using generic ramdisk.
    
    Test: m nothing
    Bug: 173742069
    Change-Id: Icff7f30b5aef9fdc59b3060316576f1f50b2a92c

--- sm8475.mk
+++ sm8475.mk
@@ -140,6 +140,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
