5697924906312031d3b5792b60dc765859c1c44a
Cosmin Tanislav <demonsingur@gmail.com>
2023-01-08 20:28:22 +0200

    sm8475-common: use generic kernel image
    Change-Id: If9e561e1becdd5771fb0e843b31a678cfce848fe

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,6 +53,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
 TARGET_KERNEL_SOURCE := kernel/motorola/sm8475
 TARGET_KERNEL_CONFIG := gki_defconfig vendor/waipio_GKI.config vendor/lineage_moto-waipio.config
