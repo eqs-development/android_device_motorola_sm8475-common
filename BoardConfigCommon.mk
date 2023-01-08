837920ca4adcbfdab9b418c7d2baf75deb3d3d7a
Cosmin Tanislav <demonsingur@gmail.com>
2023-01-08 14:07:02 +0200

    sm8475-common: use matching kernel path
    Change-Id: I1914a639c65ecf446fbdabe88b0a1133248be331

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,7 +55,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
-TARGET_KERNEL_SOURCE := kernel/motorola/sm8450
+TARGET_KERNEL_SOURCE := kernel/motorola/sm8475
 TARGET_KERNEL_CONFIG := gki_defconfig vendor/waipio_GKI.config vendor/lineage_moto-waipio.config
 
 # Kernel modules - WLAN
