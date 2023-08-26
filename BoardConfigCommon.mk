9d989ee964e7bd0542a58f294401afa3df78755e
Aaron Kling <webgeek1234@gmail.com>
2023-08-26 18:02:00 -0500

    sm8475-common: Use merge dtbs script for kernel dt
    Change-Id: I66feaef1670f33f44fdddca227b811530ae30f33

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -62,6 +62,7 @@ TARGET_KERNEL_CONFIG := \
     vendor/ext_config/moto-waipio.config \
     vendor/ext_config/moto-waipio-gki.config \
     vendor/ext_config/lineage-moto-waipio.config
+BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
