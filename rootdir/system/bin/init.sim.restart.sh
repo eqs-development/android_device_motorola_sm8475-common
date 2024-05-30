8d9c9be89428198bedf2c8bbe728e71c77538a89
Elaina <GreyElaina@outlook.com>
2024-05-30 22:23:48 +0800

    sm8475-common: Add another ID to sim restart script & adjust sleeps
    Some rtwo variants have a different ID
    
    Change-Id: I99fbc01de0a82168aa7893b26d8a828cb3b689f7

--- rootdir/system/bin/init.sim.restart.sh
+++ rootdir/system/bin/init.sim.restart.sh
@@ -5,8 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-service call phone 185 i32 0 i32 0
+sleep 1
 
-sleep 0.5
+service call phone 185 i32 0 i32 0
+service call phone 186 i32 0 i32 0
+
+sleep 2
 
 service call phone 185 i32 0 i32 1
+service call phone 186 i32 0 i32 1
