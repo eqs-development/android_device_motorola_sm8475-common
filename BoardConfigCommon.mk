2fa2ecd4ed28104ff9ad5064b07108085e8d4d33
SGCMarkus <markusornik@gmail.com>
2024-06-20 09:33:31 +0200

    sm8475-common: add back FCM file
    this contains:
    * Android 14 aidls that are not specified for FCM 6
    * Moto specific hidls/aidls
    
    Change-Id: I470bc545d3eafdd6fd9240f36cbf74b172cebc70

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,6 +150,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
