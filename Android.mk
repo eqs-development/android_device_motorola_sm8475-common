be21fdd687344374d6d2206ba1dea8de19572cba
Cosmin Tanislav <demonsingur@gmail.com>
2024-07-25 20:01:51 +0300

    sm8475-common: remove unused WiFi symlinks
    sm8475 uses qca6490.
    
    /vendor/etc/wifi/WCNSS_qcom_cfg.ini is not present on stock.
    
    Change-Id: I750aa7a9f10aab2d24cd4f69404d9ce315834aa7

--- Android.mk
+++ Android.mk
@@ -60,16 +60,9 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) $(BT_FIRMWARE_MOUNT_POI
 WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld
 $(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating wifi firmware symlinks: $@"
-	@mkdir -p $@/kiwi
 	@mkdir -p $@/qca6490
-	@mkdir -p $@/qca6750
-	$(hide) ln -sf /vendor/etc/wifi/WCNSS_qcom_cfg.ini $@/WCNSS_qcom_cfg.ini
-	$(hide) ln -sf /mnt/vendor/persist/kiwi/wlan_mac.bin $@/kiwi/wlan_mac.bin
-	$(hide) ln -sf /vendor/etc/wifi/kiwi/WCNSS_qcom_cfg.ini $@/kiwi/WCNSS_qcom_cfg.ini
 	$(hide) ln -sf /mnt/vendor/persist/qca6490/wlan_mac.bin $@/qca6490/wlan_mac.bin
 	$(hide) ln -sf /vendor/etc/wifi/qca6490/WCNSS_qcom_cfg.ini $@/qca6490/WCNSS_qcom_cfg.ini
-	$(hide) ln -sf /mnt/vendor/persist/qca6750/wlan_mac.bin $@/qca6750/wlan_mac.bin
-	$(hide) ln -sf /vendor/etc/wifi/qca6750/WCNSS_qcom_cfg.ini $@/qca6750/WCNSS_qcom_cfg.ini
 
 ALL_DEFAULT_INSTALLED_MODULES += $(WIFI_FIRMWARE_SYMLINKS)
 
