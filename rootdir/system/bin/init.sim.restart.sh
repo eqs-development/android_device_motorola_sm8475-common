83f6d3c317d6db68766c5c24516b496b366d533e
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-02 10:56:30 +0300

    Revert "sm8475-common: Add another ID to sim restart script & adjust sleeps"
    This reverts commit 8d9c9be89428198bedf2c8bbe728e71c77538a89.

--- rootdir/system/bin/init.sim.restart.sh
+++ rootdir/system/bin/init.sim.restart.sh
@@ -5,12 +5,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-sleep 1
-
 service call phone 185 i32 0 i32 0
-service call phone 186 i32 0 i32 0
 
-sleep 2
+sleep 0.5
 
 service call phone 185 i32 0 i32 1
-service call phone 186 i32 0 i32 1
