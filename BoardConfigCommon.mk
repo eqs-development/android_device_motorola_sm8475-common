ffeda463a212a2d9cd2abac6fe5a08f95078615e
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-07 19:30:36 +0300

    sm8475-common: add external modules for eqs
    Change-Id: I3f322f1f3e6a586bd1d267fb731e50e827a7ad6f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,6 +87,31 @@ TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/video-driver \
     qcom/opensource/wlan/qcacld-3.0/.qca6490
 
+TARGET_KERNEL_EXT_MODULES += \
+    motorola/drivers/mmi_annotate \
+    motorola/drivers/mmi_info \
+    motorola/drivers/power/bm_adsp_ulog \
+    motorola/drivers/power/mmi_charger \
+    motorola/drivers/power/qti_glink_charger \
+    motorola/drivers/power/qpnp_adaptive_charge \
+    motorola/drivers/misc/utag \
+    motorola/drivers/mmi_relay \
+    motorola/drivers/moto_f_mass_storage \
+    motorola/drivers/moto_f_usbnet \
+    motorola/drivers/misc/mmi_sys_temp \
+    motorola/drivers/watchdogtest \
+    motorola/drivers/regulator/wl2868c \
+    motorola/drivers/sensors \
+    motorola/drivers/misc/sx937x_multi \
+    motorola/drivers/input/touchscreen/touchscreen_mmi \
+    motorola/drivers/input/touchscreen/goodix_berlin_mmi \
+    motorola/drivers/input/touchscreen/stmicro_mmi \
+    motorola/drivers/input/misc/rbs_fod_mmi \
+    motorola/drivers/moto_mm \
+    motorola/drivers/moto_swap \
+    motorola/drivers/nfc/st21nfc \
+    motorola/drivers/ese/st54x
+
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := taro
