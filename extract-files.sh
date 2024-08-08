aa42d169bb5089aba169bdcecba0edf8073b5bfe
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-08 18:12:01 +0300

    sm8475-common: use V33 VNDK libbase for nfc_nci.nqx.default.hw.so
    Change-Id: Ia3ea4663aa9d22355ff43c4b1c25535f2afeb749

--- extract-files.sh
+++ extract-files.sh
@@ -86,6 +86,9 @@ function blob_fixup() {
         vendor/lib64/vendor.qti.hardware.qxr-V1-ndk_platform.so)
             ${PATCHELF} --replace-needed "android.hardware.common-V2-ndk_platform.so" "android.hardware.common-V2-ndk.so" "${2}"
             ;;
+        vendor/lib64/nfc_nci.nqx.default.hw.so)
+            ${PATCHELF} --replace-needed "libbase.so" "libbase-v33.so" "${2}"
+            ;;
         vendor/lib64/libancbase_rt_fusion.so)
             ${PATCHELF} --set-soname libancbase_rt_fusion.so "${2}"
             ;;
