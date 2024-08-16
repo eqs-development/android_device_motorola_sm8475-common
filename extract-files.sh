81011bbdf9088cd0ef131ae2b9aee412bf2e3805
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-16 14:47:52 +0300

    sm8475-common: add android.hardware.security.rkp-V3-ndk.so to libqtikeymint.so
    out/soong/.intermediates/vendor/motorola/sm8475-common/libqtikeymint/android_vendor_arm64_armv8-2a-dotprod_shared/e560d7b19ebf7276b3e850d3d346dec8/libqtikeymint.so: error: Unresolved symbol: _ZN4aidl7android8hardware8security7keymint30BnRemotelyProvisionedComponent12createBinderEv
    out/soong/.intermediates/vendor/motorola/sm8475-common/libqtikeymint/android_vendor_arm64_armv8-2a-dotprod_shared/e560d7b19ebf7276b3e850d3d346dec8/libqtikeymint.so: error: Unresolved symbol: _ZN4aidl7android8hardware8security7keymint30BnRemotelyProvisionedComponent16getInterfaceHashEPNSt3__112basic_stringIcNS5_11char_traitsIcEENS5_9allocatorIcEEEE
    out/soong/.intermediates/vendor/motorola/sm8475-common/libqtikeymint/android_vendor_arm64_armv8-2a-dotprod_shared/e560d7b19ebf7276b3e850d3d346dec8/libqtikeymint.so: error: Unresolved symbol: _ZN4aidl7android8hardware8security7keymint30BnRemotelyProvisionedComponent19getInterfaceVersionEPi
    out/soong/.intermediates/vendor/motorola/sm8475-common/libqtikeymint/android_vendor_arm64_armv8-2a-dotprod_shared/e560d7b19ebf7276b3e850d3d346dec8/libqtikeymint.so: error: Unresolved symbol: _ZN4aidl7android8hardware8security7keymint30BnRemotelyProvisionedComponentC2Ev
    out/soong/.intermediates/vendor/motorola/sm8475-common/libqtikeymint/android_vendor_arm64_armv8-2a-dotprod_shared/e560d7b19ebf7276b3e850d3d346dec8/libqtikeymint.so: error: Unresolved symbol: _ZN4aidl7android8hardware8security7keymint30BnRemotelyProvisionedComponentD2Ev
    
    Change-Id: I40deea9be969c5a9e5e8d944cf01a46d22b32494

--- extract-files.sh
+++ extract-files.sh
@@ -77,6 +77,7 @@ function blob_fixup() {
             ${PATCHELF} --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V1-ndk.so" "${2}"
             ${PATCHELF} --replace-needed "android.hardware.security.secureclock-V1-ndk_platform.so" "android.hardware.security.secureclock-V1-ndk.so" "${2}"
             ${PATCHELF} --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "${2}"
+            grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
         vendor/lib64/libdlbdsservice.so | vendor/lib64/soundfx/libswdap.so)
             ${PATCHELF} --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
