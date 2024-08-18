ec34e058d64ec6920dacd515a5505c965845c412
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 12:04:51 +0300

    sm8475-common: merge duplicate fixup cases
    Change-Id: Idddb49473ad38a0739e7cf9e4ec6b9221dbb601b

--- extract-files.sh
+++ extract-files.sh
@@ -67,12 +67,7 @@ function blob_fixup() {
         vendor/lib64/libcamximageformatutils.so)
             ${PATCHELF} --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
             ;;
-        vendor/bin/hw/android.hardware.security.keymint-service-qti)
-            ${PATCHELF} --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V1-ndk.so" "${2}"
-            ${PATCHELF} --replace-needed "android.hardware.security.secureclock-V1-ndk_platform.so" "android.hardware.security.secureclock-V1-ndk.so" "${2}"
-            ${PATCHELF} --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "${2}"
-            grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
-            ;;
+        vendor/bin/hw/android.hardware.security.keymint-service-qti | \
         vendor/lib64/libqtikeymint.so)
             ${PATCHELF} --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V1-ndk.so" "${2}"
             ${PATCHELF} --replace-needed "android.hardware.security.secureclock-V1-ndk_platform.so" "android.hardware.security.secureclock-V1-ndk.so" "${2}"
